a1ceb208b208e83582249403cf7bf0ae535db8de
Tomas Kvasnicka <tomas.kvasnicka@cdn77.com>
2016-10-10 23:27:08 +0200

    first release version
    - Follow link headers
    - Search in comments
    - Return value which can be useful in case of debug

--- 01/__main__.py
+++ 01/__main__.py
@@ -18,35 +18,77 @@ import re
     - requirements: https://edux.fit.cvut.cz/courses/MI-PYT/tutorials/01_requests_click
     - additional functionality:
         - use conditional HTTP requests where possible
-        - follow Link headers if pagination is applied (IN PROGRESS)
+        - follow Link headers if pagination is applied
         - logfile output
         - check repository existence (NOT IMPLEMENTED YET)
         - list issues for all repositories if none was given (NOT IMPLEMENTED YET)
     - not catching python exceptions while eg. opening files -> if such things fail my superb error msg won't save it
     - in the label file do not use RE that interfere with what configparser.ConfigParser() is expecting
         - I guess '[' and ']' might hurt it
+    - already labeled issues do not get checked again
+        - speeds up the process in case of large number of issues
+        - comments added after issue has been labeled are excluded from checking
 """
 
 
-def process_response(response, request, labels, output, link):
+def download_comments(comments_url, request, args):
+    ret = []
+
+    response = requests.get(comments_url, headers=request['headers'])
+    if 'link' in response.headers:
+        links = response.headers['link'].split(',')
+        links = {x.split("rel=")[1].strip('"'): x.split(';')[0].strip('<').strip('>') for x in links}
+
+        if 'next' in links:
+            ret += download_comments(links['next'], request, args)
+
+    for comment in response.json():
+        ret += [comment['body']]
+
+    return ret
+
+
+def process_response(response, request, labels, args):
     """ process github API response with issues """
 
-    # TODO: Handle Link header
+    ret = []
+
+    if 'link' in response.headers:
+        links = response.headers['link'].split(',')
+        links = {x.split("rel=")[1].strip('"'): x.split(';')[0].strip('<').strip('>') for x in links}
+
+        if 'next' in links:
+            r = requests.get(links['next'], headers=request['headers'])
+            ret += process_response(r, request, labels, args)
+
     for issue in response.json():
         if not issue['labels']:
             issue['labels'] = [label for regexp, label in labels.items()
                                if re.search(regexp, issue['title']) or re.search(regexp, issue['body'])]
 
-            if issue['labels']:
-                del issue['assignee']  # GH APIv3 requires this
-                r = requests.patch(request['api'] + request['user'] + '/' + request['repo'] + '/issues/' +
-                                   str(issue['number']), json=issue, headers=request['headers'])
+            if args['comments']:
+                comments = download_comments(issue['comments_url'], request, args)
 
-                if r.status_code != 200:
-                    print("Editing labels failed:", str(r.status_code), '/', str(r.json()), file=output)
-                else:
-                    print("Patched issue", str(issue['number']), "-", issue['title'], "with labels:",
-                          str(issue['labels']), file=output)
+                if comments:
+                    issue['labels'] += [label for comment in comments for regexp, label in labels.items()
+                                        if re.search(regexp, comment)]
+
+            if not issue['labels']:
+                issue['labels'] = [args['default_label']]
+
+            del issue['assignee']  # GH APIv3 requires this
+            r = requests.patch(request['api'] + request['user'] + '/' + args['repo'] + '/issues/' +
+                               str(issue['number']), json=issue, headers=request['headers'])
+
+            if r.status_code != 200:
+                print("Editing labels failed:", str(r.status_code), '/', str(r.json()), file=args['output'])
+                ret += [(False, response, r)]
+            else:
+                print("Patched issue", str(issue['number']), "-", issue['title'], "with labels:",
+                      str(issue['labels']), file=args['output'])
+                ret += [(True, response, r)]
+
+    return ret
 
 
 def parse_file(file):
@@ -64,16 +106,17 @@ def parse_file(file):
 def main(args):
     """ get issues from github and label them """
 
+    ret = []
     user = 'mi-pyt-label-robot'
     api = 'https://api.github.com/repos/'
     regexp = {re.compile(r, re.IGNORECASE): v for r, v in args['labels'].items()}
     headers = {'Authorization': 'token ' + args['token'], 'User-Agent': user}
 
-    i = 0  # FIXME: Change to 'while True' if script is supposed to run for ever
+    i = 0  # FIXME: Change to 'while True' if script is supposed to run forever
     while i < 3:
         i += 1
 
-        # When using requests.Session() GH sometimes returns something wrong, requests cannot handle it and
+        # When using requests.Session() GH sometimes returns something wrong, requests are unable to handle it and
         # requests.exceptions.ConnectionError: ('Connection aborted.', BadStatusLine("''",)) is raised.
         #
         # See http://stackoverflow.com/questions/25326616/unexpected-keyword-argument-buffering-python-client
@@ -86,13 +129,18 @@ def main(args):
             headers['If-None-Match'] = response.headers['etag']  # if 'etag' in response.headers else None
 
             if response.status_code == 200:
-                request = {'api': api, 'user': user, 'repo': args['repo'], 'headers': headers}
-                process_response(response, request, regexp, args['output'], 'link' in response.headers)
+                request = {'api': api, 'user': user, 'headers': headers}
+                ret += process_response(response, request, regexp, args)
             else:
                 print('Fetching issues for', args['repo'], 'failed:', str(response.status_code), '/', response.text,
                       file=args['output'])
+        else:
+            print("GitHub output unchanged, no action needed", file=args['output'])
 
         time.sleep(args['interval'])
+
+    # from pprint import pprint as pp
+    # pp(ret)
 
 
 @click.command()
@@ -101,7 +149,7 @@ def main(args):
 @click.option('--label-file', default='labels.cfg', help='path to label definitions file')
 @click.option('--interval', default=10, help='how often to check for issues; in sec')
 @click.option('--default-label', default='take-a-look-personally', help='default label')
-@click.option('--comments', default=False, help='check comments')
+@click.option('--comments', default=True, help='check comments')
 @click.option('--output', default=None, help='path to file used instead of stdout')
 def parse_args(repo, auth_file, label_file, interval, default_label, comments, output):
     """ parse command line arguments """
