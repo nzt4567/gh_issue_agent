95fa5124d0de48e436c0a563eeb506e548e44fab
Tomas Kvasnicka <kvasntom@fit.cvut.cz>
2016-11-09 11:22:17 +0100

    fixing pypi
    As pypi doesnt allow to reupload package with the same version
    I had to change the package to v0.5.1 - code is the same as in
    v0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README') as f:
  
 setup(
     name='gh_issue_agent',
-    version='0.5',
+    version='0.5.1',
     description='Automatic Labels for GitHub Issues',
     author='Tomas Kvasnicka',
     author_email='nzt4567@gmx.com',
